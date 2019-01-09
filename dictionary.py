2fca1d10d152e9efbbc0c0d7c815453b5cfe30b7
Kyle Knudson <kyle.knudson2015@gmail.com>
2019-01-09 14:33:43 -0600

    Implements function that returns the definition of the word

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,17 @@
 '''
 This is an interactive dictionary
 '''
+
+import json
+data = json.load(open("data.json"))
+
+def dict_search(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+    else:
+        return "The word does not exist. Double check the word"
+
+word = input('What word would you like to look up?')
+definition = dict_search(word)
+print(definition)
