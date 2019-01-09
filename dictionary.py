8325e327da508034ec97ba3d3aae1352ee19e3a2
Kyle Knudson <kyle.knudson2015@gmail.com>
2019-01-09 14:45:47 -0600

    implemented close matches determiner

--- dictionary.py
+++ dictionary.py
@@ -3,12 +3,15 @@ This is an interactive dictionary
 '''
 
 import json
+from difflib import SequenceMatcher, get_close_matches
 data = json.load(open("data.json"))
 
 def dict_search(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        return "Did you mean %s instead?" % get_close_matches(word, data.keys())[0]
     else:
         return "The word does not exist. Double check the word"
 
