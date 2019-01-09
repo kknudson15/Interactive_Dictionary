48e9e5c24221ca47f1ab0b15b8d5a270d066f62d
Kyle Knudson <kyle.knudson2015@gmail.com>
2019-01-09 15:08:58 -0600

    Commenting code

--- dictionary.py
+++ dictionary.py
@@ -1,12 +1,27 @@
 '''
-This is an interactive dictionary
+This is an interactive dictionary Program
+built by Kyle Knudson, 2019
+Built while working through UDEMY course
+
+The user is able to enter a word and the dictionary returns the definition 
+or multiple definitions depending upon the number of definitions that the 
+word has. 
+The dictionary also uses close matches to offer suggestions to the user
+to help deal with misspellings.  
 '''
 
 import json
 from difflib import SequenceMatcher, get_close_matches
+#Reads in the data from the data.json file to a python dictionary
 data = json.load(open("data.json"))
 
+
 def dict_search(word):
+    '''
+    The function handles the lookup of the word that the user enters.
+    Inputs: A string word that the user is trying to lookup
+    Outputs: returns a list of definitions or a string with a message
+    '''
     word = word.lower()
     if word in data:
         return data[word]
@@ -22,9 +37,11 @@ def dict_search(word):
     else:
         return "The word does not exist. Double check the word"
 
+#main program operation
 word = input('What word would you like to look up? ')
 definition = dict_search(word)
 
+#Output optimization
 if type(definition) == list:
     for definition in definition:
         print(definition)
